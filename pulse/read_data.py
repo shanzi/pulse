de25fb04c1e03fc60b913d4124a7eec56d2e3173
chase <chase@local>
2014-01-12 12:49:42 +0800

    multi class classify

--- pulse/read_data.py
+++ pulse/read_data.py
@@ -16,8 +16,8 @@ def zip_data(data_list):
 def to_data_set(labels, samples):
     ds = dataset_wizard(samples, targets=labels)
     len_ = len(labels)
-    a = len_/2
-    runtype = a*[0, 1]
+    a = len_/5
+    runtype = a*range(5)
     if len(runtype)<len_: runtype += [1]*(len_-len(runtype))
     ds.sa['runtype']=runtype
     return ds
@@ -27,8 +27,7 @@ def train_data(dir_root):
     abs_dir_root = os.path.abspath(dir_root)
 
     def walk_train_dir(train_data, dir_name, files):
-        dirname = os.path.split(dir_name)[-1].strip()
-        label = False if dirname=='normal' else True
+        label = os.path.split(dir_name)[-1].strip()
         for file_ in files:
             name, ext = os.path.splitext(file_)
             if ext == '.mat':
@@ -40,12 +39,10 @@ def train_data(dir_root):
                     beats = val[DATA_FIELD]
                     peak = val[PEAK_FIELD]
                     beats = np.squeeze(beats[0])
-                    peak = np.squeeze(peak[0])
-                    for i, beat in enumerate(beats):
-                        idx = peak[i]
-                        if idx > 0:
-                            sliced = beat[idx]
-                            train_data.append((label, sliced))
+                    for beat in beats:
+                        peak = np.amax(beat,0)
+                        trouph = np.amin(beat,0)
+                        train_data.append((label, peak-trouph))
 
     train_data = []
     os.path.walk(abs_dir_root, walk_train_dir, train_data)
