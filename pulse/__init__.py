3c147cf6a6124256fc294dbd6229ca741c4cd6a0
chase <chase@local>
2014-01-12 12:50:36 +0800

    svm

--- pulse/__init__.py
+++ pulse/__init__.py
@@ -2,12 +2,16 @@
 # encoding: utf-8
 
 from pulse.kNN import reduce_dim, train_kNN
+from pulse.svm import train_SVM
 from pulse.read_data import *
-
+from mvpa2.generators.partition import NFoldPartitioner
 
 def train(path):
     data = train_data(path)
     labels, samples = zip_data(data)
-    new, m = reduce_dim(labels, samples)
-    data_set = to_data_set(labels, new)
-    print train_kNN(data_set)
+    partitioner = NFoldPartitioner(attr="runtype")
+    #new, m = reduce_dim(labels, samples)
+    #data_set = to_data_set(labels, new)
+    #print train_kNN(data_set)
+    data_set = to_data_set(labels, samples)
+    print 1-train_SVM(data_set, partitioner)
--- pulse/svm.py
+++ pulse/svm.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from mvpa2.clfs.knn import kNN
+from mvpa2.clfs.svm import RbfCSVMC
 from mvpa2.measures.base import CrossValidation
-from pulse.lda import lda
+
 import numpy as np
 
-def reduce_dim(labels, samples):
-    return lda(np.array(samples), np.array(labels), 2)
-
-def train_kNN(data_set, partitioner):
-    clf = kNN()
+def train_SVM(data_set, partitioner):
+    clf = RbfCSVMC(0.8)
     clf.set_postproc(None)
     cv = CrossValidation(clf, partitioner)
     cv_results = cv(data_set)
     return np.mean(cv_results)
-
